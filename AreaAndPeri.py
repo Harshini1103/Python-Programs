2b0bfcccdc30e9a7cc72207a60ed22aeceb4dd87
Harshini1103 <harshini11122003@gmail.com>
2025-02-25 22:31:46 +0530

    Created

---
+++ AreaAndPeri.py
@@ -0,0 +1,10 @@
+import math
+r=float(input("Enter the radius value:"))
+area=math.pi*r*r
+peri=2*math.pi*r
+print("Area of a circle is:",area)
+print("Perimeter of a circle is:",peri)
+# output:
+# Enter the radius value:10
+# Area of a circle is: 314.1592653589793
+# Perimeter of a circle is: 62.83185307179586
