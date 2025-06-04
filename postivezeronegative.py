30d803472826fc0dff84ab314e6770ee9ef2d07a
Harshini P <harshini11122003@gmail.com>
2025-06-04 20:32:30 +0530

    Create postivezeronegative.py

---
+++ postivezeronegative.py
@@ -0,0 +1,14 @@
+num=float(input("Enter a number:"))
+if(num>0):
+    print("its a positive number")
+elif num==0:
+    print("its a Zero")
+else:
+    print("Its a negative numbers")
+# Enter a number:5
+# its a positive number
+# Enter a number:0
+# its a Zero
+# Enter a number:-6
+# Its a negative numbers
+
